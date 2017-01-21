edb612a850877e676489081d176b22d02761aab1
Rick Lancee <rickondraw@gmail.com>
2017-01-21 19:17:41 +0100

    add webpack production config

--- core.js
+++ core.js
@@ -7,7 +7,7 @@ import loader from './components/loader';
 import login from './components/login';
 import { default as toaster } from './components/toast';
 
-export default async function core(services) {
+export default function core(services) {
   console.info('initialize with ', services);
 
   const provider = services.providers.mal;
@@ -17,55 +17,56 @@ export default async function core(services) {
   const loginComponent = login(services, provider, '[data-ref="login"]');
   loginComponent.register();
 
-  const user = await loginComponent.loginOrPrompt();
+  // Login remebered user or prompt a login, when done render the lists.
+  loginComponent.loginOrPrompt().then(user => {
+    let list = [];
+    const cachedList = services.storage.getItem(`app.${user.username}.list`);
 
-  let list = [];
-  const cachedList = services.storage.getItem(`app.${user.username}.list`);
-
-  if (cachedList) {
-    console.info('Using cached list...');
-    list = JSON.parse(cachedList);
-  }
-
-  actions(services, provider, user, list);
-
-  // Register components.
-  console.log('Registering components...');
-  const toast = toaster(services, '[data-ref="toast"]');
-  const appLoadIcon = loader(services, '[data-ref="loader"]');
-  const container = cardContainer(services, card, '[data-ref="cardContainer"]', list);
-
-  // Bootstrap components
-  appLoadIcon.register();
-  container.register();
-  toast.register();
-
-  services.bus.emit('app:isDoingSomeWork');
-  provider.list.get().then(data => {
-    console.info('Fetched list from provider.');
-
-    services.bus.emit('app:isDoneDoingSomeWork');
-
-    // Update the container state list only if there are changes
-    const json = JSON.stringify(data);
-
-    if (!cachedList) {
-      console.info('Updating list from empty cache');
-      services.storage.setItem(`app.${user.username}.list`, json);
-      container.updateState(data);
-      container.render();
-    } else if (cachedList !== json) {
-      console.info('List updated, resetting cache with new list');
-      services.storage.setItem(`app.${user.username}.list`, json);
-      container.updateState(data);
-      container.render();
-      toast.show('Your list on MAL was updated, changes are reflected here.', [], 3000);
-    } else {
-      console.info('List the same, using cached list');
+    if (cachedList) {
+      console.info('Using cached list...');
+      list = JSON.parse(cachedList);
     }
-  }).catch(err => {
-    services.bus.emit('app:isDoneDoingSomeWork');
-    toast.show('Failed to retrieve list from MAL. See console for errors', [], 3000);
-    console.error(err);
+
+    actions(services, provider, user, list);
+
+    // Register components.
+    console.log('Registering components...');
+    const toast = toaster(services, '[data-ref="toast"]');
+    const appLoadIcon = loader(services, '[data-ref="loader"]');
+    const container = cardContainer(services, card, '[data-ref="cardContainer"]', list);
+
+    // Bootstrap components
+    appLoadIcon.register();
+    container.register();
+    toast.register();
+
+    services.bus.emit('app:isDoingSomeWork');
+    provider.list.get().then(data => {
+      console.info('Fetched list from provider.');
+
+      services.bus.emit('app:isDoneDoingSomeWork');
+
+      // Update the container state list only if there are changes
+      const json = JSON.stringify(data);
+
+      if (!cachedList) {
+        console.info('Updating list from empty cache');
+        services.storage.setItem(`app.${user.username}.list`, json);
+        container.updateState(data);
+        container.render();
+      } else if (cachedList !== json) {
+        console.info('List updated, resetting cache with new list');
+        services.storage.setItem(`app.${user.username}.list`, json);
+        container.updateState(data);
+        container.render();
+        toast.show('Your list on MAL was updated, changes are reflected here.', [], 3000);
+      } else {
+        console.info('List the same, using cached list');
+      }
+    }).catch(err => {
+      services.bus.emit('app:isDoneDoingSomeWork');
+      toast.show('Failed to retrieve list from MAL. See console for errors', [], 3000);
+      console.error(err);
+    });
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ module.exports = {
   entry: './app.js',
   output: {
     filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader?presets[]=es2015'
+      }
+    ]
   }
 }
